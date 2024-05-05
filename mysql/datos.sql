985fd0f4c4a39d59b6df2fd20418a23acd7831e5
rochigonzalezc <94115157+rochigonzalezc@users.noreply.github.com>
2024-05-05 15:50:58 +0200

    agrego contraseñas hasheadas

--- mysql/datos.sql
+++ mysql/datos.sql
@@ -1,10 +1,18 @@
 -- Insertar datos en la tabla de Usuarios
+
+-- La contraseña de los usuarios es nombre y apellido en minúsculas, sin espacios
+-- La contraseña de Juan Perez es 'juanperez'
+-- La contraseña de Ana Lopez es 'analopez'
+-- La contraseña de Carlos Ruiz es 'carlosruiz'
+-- La contraseña de Julian Fernandez es 'julianfernandez'
+-- La contraseña de Marta Sanchez es 'martasanchez'
+
 INSERT INTO Usuarios (Nombre, Email, HashContraseña) VALUES
-('Juan Perez', 'juan.perez@gmail.com', 'juanperez'),
-('Ana Lopez', 'ana.lopez@gmail.com', 'analopez'),
-('Carlos Ruiz', 'carlos.ruiz@gmail.com', 'carlosruiz'),
-('Julian Fernandez', 'julian.fernandez@gmail.com', 'julianfernandez'),
-('Marta Sanchez', 'marta.sanchez@gmail.com', 'martasanchez');
+('Juan Perez', 'juan.perez@gmail.com', '$2y$10$424qcpUBo3kLWKM3GhYt0OoUzZBlL44CVDDf/mO3NajJ8ksONQU9C'),
+('Ana Lopez', 'ana.lopez@gmail.com', '$2y$10$CTA2d6F3A.4LZV2.ru5YcOJ7nDWjVmTr.AK/OT2YM6SxDUMi4n3qC'),
+('Carlos Ruiz', 'carlos.ruiz@gmail.com', '$2y$10$0S00aPhRSuyip9aceX6X9uz3MsUr.avq7TkSrS7m77uNPyjlHv06C'),
+('Julian Fernandez', 'julian.fernandez@gmail.com', '$2y$10$F12KHPdEXHDF8.3ZmbaQbufCgP0Mu0HK3y/wfnO8TyPS3qC5I3C5O'),
+('Marta Sanchez', 'marta.sanchez@gmail.com', '$2y$10$9m7Q02rgo1domtWjwzfeQu2DIw/.7FrBdvnU7iWzMPB4RdiQLIBbG');
 
 -- Insertar datos en la tabla de Instructores
 INSERT INTO Instructores (Nombre) VALUES
